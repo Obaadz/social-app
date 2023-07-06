b255584193eb54bbb3af917439f451c55671e6a5
Obada <obada8411@gmail.com>
2023-07-06 13:02:01 +0300

    Fix 'loadEnvironment' issue

--- src/utils/loadEnvironment.ts
+++ src/utils/loadEnvironment.ts
@@ -2,7 +2,8 @@ import { config } from "dotenv";
 import envSchema from "./validators/schema/envSchema.js";
 
 export default () => {
-  config();
+  if (process.env.NODE_ENV !== "production") config({ path: ".env.local" });
+  else config();
 
   envSchema.parse(process.env);
 };

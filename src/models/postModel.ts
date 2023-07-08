9e14220e6db7c170ed87d859df0af65514a42d0d
Obada <obada8411@gmail.com>
2023-07-08 04:20:07 +0300

    Add moment package and 'createdFrom' property in post model

--- src/models/postModel.ts
+++ src/models/postModel.ts
@@ -4,6 +4,7 @@ import { IComment } from "./commentModel.js";
 import imageSchema from "../utils/validators/schema/imageSchema.js";
 import CATEGORIES from "../utils/categories.js";
 import loadEnvironment from "../utils/loadEnvironment.js";
+import moment from "moment";
 
 loadEnvironment();
 
@@ -19,59 +20,74 @@ export interface IPost extends Document {
 
 export interface IPostModel extends Model<IPost> {}
 
-const postSchema: Schema<IPost> = new mongoose.Schema({
-  caption: {
-    type: String,
-    maxlength: [
-      Number(process.env.MAX_POST_CAPTION_LENGTH),
-      `Max post length cannot exceed ${process.env.MAX_POST_CAPTION_LENGTH} characters!`,
-    ],
-    default: "",
-  },
-  image: {
-    type: String,
-    required: [true, "Image is required!"],
-    validate: [
-      (value: string) => {
-        try {
-          imageSchema.parse(value);
+const postSchema: Schema<IPost> = new mongoose.Schema(
+  {
+    caption: {
+      type: String,
+      maxlength: [
+        Number(process.env.MAX_POST_CAPTION_LENGTH),
+        `Max post length cannot exceed ${process.env.MAX_POST_CAPTION_LENGTH} characters!`,
+      ],
+      default: "",
+    },
+    image: {
+      type: String,
+      required: [true, "Image is required!"],
+      validate: [
+        (value: string) => {
+          try {
+            imageSchema.parse(value);
 
-          return true;
-        } catch (err) {
-          return false;
-        }
+            return true;
+          } catch (err) {
+            return false;
+          }
+        },
+        "Invalid url for image!",
+      ],
+    },
+    author: {
+      type: Types.ObjectId,
+      ref: "User",
+      required: [true, "Author is required!"],
+    },
+    likes: {
+      type: [Types.ObjectId],
+      ref: "User",
+      default: [],
+    },
+    category: {
+      type: String,
+      enum: CATEGORIES,
+      required: [true, "Category is required!"],
+    },
+    comments: {
+      type: [Types.ObjectId],
+      ref: "Comment",
+      default: [],
+    },
+    __v: {
+      type: Number,
+      select: false,
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      transform(doc, ret) {
+        delete ret.createdAt;
       },
-      "Invalid url for image!",
-    ],
-  },
-  author: {
-    type: Types.ObjectId,
-    ref: "User",
-    required: [true, "Author is required!"],
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  likes: {
-    type: [Types.ObjectId],
-    ref: "User",
-    default: [],
-  },
-  category: {
-    type: String,
-    enum: CATEGORIES,
-    required: [true, "Category is required!"],
-  },
-  comments: {
-    type: [Types.ObjectId],
-    ref: "Comment",
-    default: [],
-  },
-  __v: {
-    type: Number,
-    select: false,
-  },
+    },
+    id: false,
+    timestamps: { createdAt: true, updatedAt: false },
+  }
+);
+
+postSchema.virtual("createdFrom").get(function (this: IPost) {
+  const currentTime = moment();
+  const pastTime = moment(this.createdAt);
+
+  return pastTime.from(currentTime);
 });
 
 const PostModel: IPostModel = mongoose.model<IPost, IPostModel>("Post", postSchema);

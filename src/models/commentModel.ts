cf74cde73f5914f8c4aa73f7a855e4474cbb6a81
Obada <obada8411@gmail.com>
2023-07-10 02:45:19 +0300

    Add post property to comment model

--- src/models/commentModel.ts
+++ src/models/commentModel.ts
@@ -1,34 +1,47 @@
 import mongoose, { Document, Model, Schema, Types } from "mongoose";
 import { IUser } from "./userModel.js";
+import { IPost } from "./postModel.js";
 
 export interface IComment extends Document {
   content: string;
   author: IUser | Types.ObjectId;
+  post: IPost | Types.ObjectId;
   createdAt: Date;
 }
 
 export interface ICommentModel extends Model<IComment> {}
 
-const commentSchema: Schema<IComment> = new mongoose.Schema({
-  content: {
-    type: String,
-    required: [true, "Content is required!"],
-    trim: true,
+const commentSchema: Schema<IComment> = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: [true, "Content is required!"],
+      trim: true,
+    },
+    author: {
+      type: Types.ObjectId,
+      ref: "User",
+      required: [true, "Author is required!"],
+    },
+    post: {
+      type: Types.ObjectId,
+      ref: "Post",
+      required: [true, "Post is required!"],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    __v: {
+      type: Number,
+      select: false,
+    },
   },
-  author: {
-    type: Types.ObjectId,
-    ref: "User",
-    required: [true, "Author is required!"],
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  __v: {
-    type: Number,
-    select: false,
-  },
-});
+  {
+    id: false,
+    timestamps: { createdAt: true, updatedAt: false },
+  }
+);
 
 const CommentModel: ICommentModel = mongoose.model<IComment, ICommentModel>(
   "Comment",

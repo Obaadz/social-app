d2ea79bd54daa95aa5a5d01c06b6a688904d7d4b
Obada <obada8411@gmail.com>
2023-07-10 02:46:06 +0300

    Change error message for object id zod schema

--- src/utils/validators/schema/objectIdSchema.ts
+++ src/utils/validators/schema/objectIdSchema.ts
@@ -1,6 +1,6 @@
 import { isValidObjectId } from "mongoose";
 import { z } from "zod";
 
-export default z.any({ required_error: "User ID is required!" }).refine((val) => {
+export default z.any({ required_error: "ID is required!" }).refine((val) => {
   return isValidObjectId(val);
-}, "Invalid user id");
+}, "Invalid id");
